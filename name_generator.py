38c25398ba15a554277ee5d66f0a29a0bb33000c
Jim Cooke <jimcoo2003@yahoo.ca>
2020-11-28 22:04:01 -0500

    Add files via upload

--- name_generator.py
+++ name_generator.py
@@ -3,6 +3,7 @@
 import random
 import time
 import math
+import array
 
 #build 27x27 table of letters in names and letters that follow
 # _ to z
@@ -11,13 +12,17 @@ import math
 # startwith[] = total number of pairs that start with that letter
 #use random number rand()*startwith[n] to find the letter after n
 
-current_names = [_carol_alice_bob_bill_]
+current_names = '_carol_alice_bob_bill_'
 
-letters = [_abcdefghijklmnopqrstuvwxyz]
+letters = '_abcdefghijklmnopqrstuvwxyz'
 
+#fill 27x27 table
+grid = {}
 for n in range (0, 27):
     for m in range (0, 27):
-        
+        grid[(n, m)]=1
+
+
 
 
 
