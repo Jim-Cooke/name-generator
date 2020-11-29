cd2d4e327f7e2f4243e67c922aff3ad4593d7cee
Jim Cooke <jimcoo2003@yahoo.ca>
2020-11-28 19:46:51 -0500

    Add files via upload

---
+++ name_generator.py
@@ -0,0 +1,28 @@
+# Name Generator program
+# import library with random function
+import random
+import time
+import math
+
+#build 27x27 table of letters in names and letters that follow
+# _ to z
+# (a,b)= number of times b comes after a in words
+# (a,b)=1 initial value
+# startwith[] = total number of pairs that start with that letter
+#use random number rand()*startwith[n] to find the letter after n
+
+current_names = [_carol_alice_bob_bill_]
+
+letters = [_abcdefghijklmnopqrstuvwxyz]
+
+for n in range (0, 27):
+    for m in range (0, 27):
+        
+
+
+
+    
+        
+    
+
+
