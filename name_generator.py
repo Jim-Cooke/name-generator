364d165b90afd046c53cbd51ec15dba1a1573b2e
Jim Cooke <jimcoo2003@yahoo.ca>
2020-12-18 21:23:06 -0500

    Add files via upload

--- name_generator.py
+++ name_generator.py
@@ -14,6 +14,16 @@ import array
 # startwith[] = total number of pairs that start with that letter
 #use random number rand()*startwith[n] to find the letter after n
 
+#my random function
+def random_1 ():
+    r = random.random()
+    t = time.time()
+    # print(r, " ", t)
+    rs = r + t
+    # print(rs)
+    rs = rs - math.floor(rs)
+    return rs
+
 current_names = '_carol_alice_bob_bill_'
 
 letters = '_abcdefghijklmnopqrstuvwxyz'
@@ -58,6 +68,22 @@ for n in range (0, len(current_names)-1):
 #    print (firstletter[p], " ", nextletter[p], " ", pairs[p])
 #print(startwith)
 
+#enter first letter of name
+newname = '_'
+randname = 'x'
+while randname != 'y' and randname != 'n':
+    randname = input("Do you want a random first letter?")
+    #print(randname)
+if randname == 'y':
+    #pick random first letter
+    currentletter = letters[int(random_1()*26)+1]
+elif randname == 'n':
+    #prompt for first letter
+    currentletter = input("Enter first letter of the name")
+
+newname = newname + currentletter
+
+#print(newname)
 
         
 
