0a4b804edc963374b58fbfbac0c53bea8222b216
Jim Cooke <jimcoo2003@yahoo.ca>
2021-02-14 21:19:37 -0500

    Add files via upload

--- name_generator.py
+++ name_generator.py
@@ -24,7 +24,15 @@ def random_1 ():
     rs = rs - math.floor(rs)
     return rs
 
-current_names = '_jim_donna_bryce_greg_joel_'
+#current_names = '_jim_donna_bryce_greg_joel_'
+#first names
+current_names = '__babe_essa_essa_cabot_ziggy_urkle_keith_flizbit_bye_mag'\
+'_russell_allen_rodney_matthew_richard_sherna_shamus_wood_barrey_pu_alan_sid'\
+'_paul_pud_eric_randy_sheri_bransio_rako_jane_craig_bruce_cindy_stevenip_sarai'\
+'_richard_alex_margaret_sandra_laura_steve_susan_paul_irene_irene_rita_rita'\
+'_rita_stephan_carl_mickey_lefke_steve_lorna_marta_lisa_marina_valerie_janet'\
+'_eayan_connie_lynn_jennifer_ron_gary_sylvia_joanne_troy_malayah_karl_bob'\
+'_mark_guy_helen_rebecca_tom_james_'
 
 letters = '_abcdefghijklmnopqrstuvwxyz'
 startwith = [0]
@@ -39,8 +47,8 @@ for n in range (0, 27):
 #    for m in range (0, 27):
 #        grid[(n, m)]=1
 
-1stletter = ['_']
-2ndletter = ['_']
+firstletter = ['_']
+secondletter = ['_']
 pairs = [0]
 #append to these three lists
 
@@ -55,58 +63,63 @@ for n in range (0, len(current_names)-1):
     # print(letter_1)
     # create table of pairs and how often they occur
     for m in range (0, len(pairs)):
-        if 1stletter[m] == letter_1 and 2ndletter[m] == letter_2:
+        if firstletter[m] == letter_1 and secondletter[m] == letter_2:
             break
-    if 1stletter[m] == letter_1 and 2ndletter[m] == letter_2:
+    if firstletter[m] == letter_1 and secondletter[m] == letter_2:
         pairs[m] = pairs[m] + 1
     else:
-        1stletter.append(letter_1)
-        2ndletter.append(letter_2)
+        firstletter.append(letter_1)
+        secondletter.append(letter_2)
         pairs.append(1)
 
 #for p in range (0, len(pairs)):
 #    print (firstletter[p], " ", nextletter[p], " ", pairs[p])
 #print(startwith)
 
-#enter first letter of name
-newname = '_'
-randname = 'x'
-while randname != 'y' and randname != 'n':
-    randname = input("Do you want a random first letter?")
-    #print(randname)
-if randname == 'y':
-    #pick random first letter
-    currentletter = letters[int(random_1()*26)+1]
-elif randname == 'n':
-    #prompt for first letter
-    currentletter = input("Enter first letter of the name")
+goagain = 'y'
+while goagain == 'y':
+    #enter first letter of name
+    newname = '_'
+    randname = 'x'
+    while randname != 'y' and randname != 'n':
+        randname = input("Do you want a random first letter? y or n")
+        print(randname)
+    if randname == 'y':
+        #pick random first letter
+        currentletter = letters[int(random_1()*26)+1]
+        #print(currentletter)
+    elif randname == 'n':
+        #prompt for first letter
+        currentletter = input("Enter first letter of the name")
 
-newname = newname + currentletter
-# generate name
-while currentletter != '_':
-    # find next letter based on pairs that start with current letter
-    # find how often pairs start with the letter
-    for q in range (0, 27):
-        if currentletter == letters[q]:
-            howoften = startwith[q]
-            break
-    if howoften == 0:
-        #pick a random next letter
-        nextletter = letters[int(random_1()*27)]
-    else:
-        #pick a random number from 1 to howoften and find nextletter
-        count = int(random_1()*howoften)+1
-        np = 0
-        while count > 0:
-            np = np+1
-            if 1stletter[np] == currentletter:
-                count = count - pairs[np]
-        nextletter = 2ndletter[np]
+    newname = newname + currentletter
+    # generate name
+    while currentletter != '_':
+        # find next letter based on pairs that start with current letter
+        # find how often pairs start with the letter
+        for q in range (0, 27):
+            if currentletter == letters[q]:
+                howoften = startwith[q]
+                #print(howoften)
+                break
+        if howoften == 0:
+            #pick a random next letter
+            nextletter = letters[int(random_1()*27)]
+            #print(nextletter)
+        else:
+            #pick a random number from 1 to howoften and find nextletter
+            count = int(random_1()*howoften)+1
+            np = 0
+            while count > 0:
+                np = np+1
+                if firstletter[np] == currentletter:
+                    count = count - pairs[np]
+            nextletter = secondletter[np]
         newname = newname + nextletter
         currentletter = nextletter
         
-print(newname)
-
+    print(newname)
+    goagain = input("Do you want to generate another name? y or n")
         
 
 
